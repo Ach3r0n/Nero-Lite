2a42efbb93192da370f0e6cb270da5e9bf842c7e
Klaas Nekeman <Klaas Nekeman@2f3a1cb8-c86e-3b4f-b219-34b0bbeac45c>
2008-05-18 21:28:27 +0000

    - build configlicense.exe using latest AutoIT 3.2.12.0
    git-svn-id: svn://localhost/NeroLite2@51 2f3a1cb8-c86e-3b4f-b219-34b0bbeac45c

--- Script/AutoIT/compile_autoit.cmd
+++ Script/AutoIT/compile_autoit.cmd
@@ -1,5 +1,5 @@
 @echo off
-"%programfiles%\AutoIt3\Beta\Aut2Exe\Aut2Exe.exe" /in configlicense.au3 /out "..\..\Setup\configlicense.exe" /nopack
+"%programfiles%\AutoIt3\Aut2Exe\Aut2Exe.exe" /in configlicense.au3 /out "..\..\Setup\configlicense.exe" /nopack
 ::push to script dir
 pushd ..
 call "Include\signfile.cmd" "..\Setup\configlicense.exe"

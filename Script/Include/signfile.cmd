f38a23cdf0d672cfc1360fe485587838c1bcd148
Klaas Nekeman <Klaas Nekeman@2f3a1cb8-c86e-3b4f-b219-34b0bbeac45c>
2009-06-29 14:15:01 +0000

    Update Personal Certificate
    git-svn-id: svn://localhost/NeroLite2@151 2f3a1cb8-c86e-3b4f-b219-34b0bbeac45c

--- Script/Include/signfile.cmd
+++ Script/Include/signfile.cmd
@@ -1,1 +1,1 @@
-Tools\CodeSigning\signcode.exe -sha1 "36d47a6bb4eaf4897f82df19442e09d90db3a8bd" -i http://updatepack.nl -t http://timestamp.verisign.com/scripts/timstamp.dll %1
+Tools\CodeSigning\signcode.exe -sha1 "bcf99dc84178b5df3a4bd6d64d2f56004baa77aa" -i http://updatepack.nl -t http://timestamp.verisign.com/scripts/timstamp.dll %1

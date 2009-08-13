0ca488a213b7325115edab487d82d634f7f9bfb4
Klaas Nekeman <Klaas Nekeman@2f3a1cb8-c86e-3b4f-b219-34b0bbeac45c>
2009-08-13 01:15:32 +0000

    Fix prepare script
    git-svn-id: svn://localhost/NeroLite2@161 2f3a1cb8-c86e-3b4f-b219-34b0bbeac45c

--- NeroLite_prepare.cmd
+++ NeroLite_prepare.cmd
@@ -1,6 +1,6 @@
 @echo off
 ::get Nero Version
-rem call Script\Include\getNeroVersion.cmd
+call Script\Include\getNeroVersion.cmd
 
 Tools\7-zip\7za x -y %nero_setup%.exe -o"%TEMP%\Nero%neroversion:.=%"
 echo.
